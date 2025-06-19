e7e903bae251271342939955e5dd52c96108b208
Jose Quesada <joseprogdp@Mac.lan>
2025-06-19 09:30:37 -0400

    added get mouse position command

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,27 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+#[macro_use]
+extern crate serde;
+
+use mouse_position::mouse_position;
+use mouse_position::Mouse;
+
+#[derive(Default, Serialize)]
+struct Position {
+    x: i32,
+    y: i32,
+}
+
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn get_mouse_position() -> Position {
+    match Mouse::get_mouse_position() {
+        Mouse::Position { x, y } => Position { x, y },
+        Mouse::Error => Position::default(),
+    }
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![get_mouse_position])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
