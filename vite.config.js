67ee6342d47ed98bf0154f3e3ad9dc42a6692be9
Jose Quesada <joseprogdp@Mac.lan>
2025-07-12 19:59:30 -0400

    added entry files when building frontend for production

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,18 @@
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
 import { defineConfig } from "vite";
 import tailwindPlugin from "@tailwindcss/vite";
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        crosshair: resolve(__dirname, "./crosshair.html"),
+        settings: resolve(__dirname, "./settings.html"),
+      },
+    },
+  },
   plugins: [tailwindPlugin()],
 });
